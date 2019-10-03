3b341e0fec2c27614bb7051fe01560bed71cd6ed
Jake Morrison <jake@cogini.com>
2019-10-03 10:12:56 +0800

    Generate project

---
+++ lib/file_config_rocksdb.ex
@@ -0,0 +1,18 @@
+defmodule FileConfigRocksdb do
+  @moduledoc """
+  Documentation for FileConfigRocksdb.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FileConfigRocksdb.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/file_config_rocksdb_test.exs
@@ -0,0 +1,8 @@
+defmodule FileConfigRocksdbTest do
+  use ExUnit.Case
+  doctest FileConfigRocksdb
+
+  test "greets the world" do
+    assert FileConfigRocksdb.hello() == :world
+  end
+end
