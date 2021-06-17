4c3fa790b1f2a4a6582621ca0737db1bf1cc8cd0
Jake Morrison <jake@cogini.com>
2021-06-17 15:33:24 -0500

    Update code to latest API

--- lib/file_config_rocksdb/handler/csv.ex
+++ lib/file_config_rocksdb/handler/csv.ex
@@ -3,96 +3,148 @@ defmodule FileConfigRocksdb.Handler.Csv do
   @app :file_config_rocksdb
 
   NimbleCSV.define(FileConfigRocksdb.Handler.Csv.Parser, separator: "\t", escape: "\0")
-
   alias FileConfigRocksdb.Handler.Csv.Parser
 
   require Logger
 
   alias FileConfig.Loader
+  # alias FileConfig.Lib
 
   # @impl true
-  @spec lookup(Loader.table_state, term) :: term
-  def lookup(table, key) do
-    %{id: tid, name: name, db_path: db_path, data_parser: data_parser} = table
+  @spec lookup(Loader.table_state(), term()) :: term()
+  def lookup(%{id: tid, name: name, parser: parser} = state, key) do
+    db_path = state[:db_path] || []
+    parser_opts = state[:parser_opts] || []
+
     case :ets.lookup(tid, key) do
-      [{^key, :undefined}] -> # Cached "not found" result from db
-        :undefined;
-      [{^key, value}] -> # Found result
+      [{_key, :undefined}] ->
+        # Cached "not found" result
+        :undefined
+
+      [{_key, value}] ->
+        # Cached result
         {:ok, value}
+
       [] ->
+        # Not found
         # TODO: somehow reuse db handle
         {:ok, db} = :rocksdb.open(db_path, create_if_missing: false)
-        return = case :rocksdb.get(db, key, []) do
-          {:ok, value} ->
-            parsed_value = data_parser.parse_value(name, key, value)
-            # Cache parsed value
-            true = :ets.insert(tid, [{key, parsed_value}])
-            {:ok, parsed_value}
-          :not_found ->
-            # Cache not found result
-            true = :ets.insert(tid, [{key, :undefined}])
-            :undefined
-          error ->
-            Logger.warning("Error reading from rocksdb #{name} #{key}: #{inspect error}")
-            :undefined
-        end
+
+        return =
+          case :rocksdb.get(db, key, []) do
+            {:ok, bin} ->
+              case parser.decode(bin, parser_opts) do
+                {:ok, value} ->
+                  # Cache parsed value
+                  true = :ets.insert(tid, [{key, value}])
+                  {:ok, value}
+
+                {:error, reason} ->
+                  Logger.debug("Error parsing table #{name} key #{key}: #{inspect(reason)}")
+                  {:ok, bin}
+              end
+
+            :not_found ->
+              # Cache "not found" result
+              true = :ets.insert(tid, [{key, :undefined}])
+              :undefined
+
+            error ->
+              Logger.warning("Error reading from rocksdb #{name} #{key}: #{inspect(error)}")
+              :undefined
+          end
+
         :ok = :rocksdb.close(db)
         return
     end
   end
 
   # @impl true
-  @spec load_update(Loader.name, Loader.update, :ets.tid) :: Loader.table_state
+  @spec load_update(Loader.name(), Loader.update(), :ets.tid()) :: Loader.table_state()
   def load_update(name, update, tid) do
     # Assume updated files contain all records
     {path, _state} = hd(update.files)
+    config = update.config
 
     db_path = db_path(name)
-    config = update.config
     chunk_size = config[:chunk_size] || 100
 
     if update_db?(db_path, update.mod) do
-      Logger.debug("Loading #{name} rocksdb #{path} #{db_path}")
+      Logger.debug("Loading #{name} #{path} #{db_path}")
       {time, {:ok, rec}} = :timer.tc(&parse_file/3, [path, db_path, chunk_size])
-      Logger.notice("Loaded #{name} rocksdb #{path} #{rec} rec #{time / 1_000_000} sec")
+      Logger.info("Loaded #{name} #{path} #{rec} rec #{time / 1_000_000} sec")
     else
-      Logger.notice("Loaded #{name} rocksdb #{db_path} up to date")
+      Logger.info("Loaded #{name} #{db_path} up to date")
     end
 
-    %{
-      name: name,
-      id: tid,
-      mod: update.mod,
-      handler: __MODULE__,
-      data_parser: config[:data_parser],
-      db_path: to_charlist(db_path),
-      chunk_size: chunk_size
-    }
+    Map.merge(
+      %{
+        name: name,
+        id: tid,
+        mod: update.mod,
+        handler: __MODULE__,
+        db_path: to_charlist(db_path),
+        chunk_size: chunk_size
+      },
+      Map.take(config, [:parser, :parser_opts, :commit_cycle])
+    )
   end
 
   # @impl true
-  @spec insert_records(Loader.table_state, [tuple]) :: :ok
-  def insert_records(table, records) do
-    {:ok, db} = :rocksdb.open(table.db_path, create_if_missing: false)
+  @spec insert_records(Loader.table_state(), {term(), term()} | [{term(), term()}]) :: true
+  def insert_records(state, records) when is_list(records) do
+    {:ok, db} = :rocksdb.open(state.db_path, create_if_missing: false)
 
     records
-    |> Enum.sort
-    |> Enum.chunk_every(table.chunk_size)
-    |> Enum.map(&insert_chunk(&1, table, db))
+    |> Enum.sort()
+    |> Enum.chunk_every(state.chunk_size)
+    |> Enum.map(&insert_chunk(&1, state, db))
 
     :ok = :rocksdb.close(db)
+    true
   end
+
+  def insert_records(state, record) when is_tuple(record), do: insert_records(state, [record])
 
   # Internal functions
 
-  defp parse_file(path, db_path, chunk_size) do
-    {topen, {:ok, db}} = :timer.tc(:rocksdb, :open, [to_charlist(db_path), [create_if_missing: true]])
+  @spec update_db?(Path.t(), :calendar.datetime()) :: boolean()
+  defp update_db?(path, update_mtime) do
+    case File.stat(path) do
+      {:ok, _dir_stat} ->
+        case File.stat(Path.join(path, "CURRENT")) do
+          {:ok, %{mtime: file_mtime}} ->
+            file_mtime < update_mtime
 
-    stream = path
-    |> File.stream!(read_ahead: 100_000)
-    |> Parser.parse_stream(headers: false)
-    |> Stream.chunk_every(chunk_size)
-    |> Stream.map(&write_chunk(&1, db))
+          {:error, _reason} ->
+            true
+        end
+
+      # case File.ls(path) do
+      #   {:ok, files} ->
+      #     file_times = for file <- files, file_path <- Path.join(path, file),
+      #       {:ok, %{mtime: file_mtime}} <- File.stat(file_path), do: file_mtime
+      #     Enum.all?(file_times, &(&1 < mod))
+      #   {:error, reason} ->
+      #     Logger.warning("Error reading path #{path}: #{reason}")
+      #     true
+      # end
+      {:error, :enoent} ->
+        true
+    end
+  end
+
+  @spec parse_file(Path.t(), Path.t(), non_neg_integer()) :: {:ok, non_neg_integer()}
+  defp parse_file(path, db_path, chunk_size) do
+    {topen, {:ok, db}} =
+      :timer.tc(:rocksdb, :open, [to_charlist(db_path), [create_if_missing: true]])
+
+    stream =
+      path
+      |> File.stream!(read_ahead: 100_000)
+      |> Parser.parse_stream(skip_headers: false)
+      |> Stream.chunk_every(chunk_size)
+      |> Stream.map(&write_chunk(&1, db))
 
     start_time = :os.timestamp()
     results = Enum.to_list(stream)
@@ -101,7 +153,12 @@ defmodule FileConfigRocksdb.Handler.Csv do
     # :ok = :rocksdb.close(db)
     {tclose, :ok} = :timer.tc(:rocksdb, :close, [db])
 
-    Logger.debug("open time: #{topen / 1_000_000}, process time: #{process_duration}, close time: #{tclose / 1_000_000}")
+    Logger.debug(
+      "open time: #{topen / 1_000_000}, process time: #{process_duration}, close time: #{
+        tclose / 1_000_000
+      }"
+    )
+
     # Logger.debug("results: #{inspect results}")
 
     rec = Enum.reduce(results, 0, fn {count, _duration}, acc -> acc + count end)
@@ -109,13 +166,13 @@ defmodule FileConfigRocksdb.Handler.Csv do
   end
 
   # @doc "Get path to db for name"
-  @spec db_path(atom) :: Path.t
+  @spec db_path(atom()) :: Path.t()
   defp db_path(name) do
     state_dir = Application.get_env(@app, :state_dir, "/var/lib/file_config")
     Path.join(state_dir, to_string(name))
   end
 
-  @spec write_chunk(list(tuple), :rocksdb.db_handle) :: {non_neg_integer, non_neg_integer}
+  @spec write_chunk(list(tuple()), :rocksdb.db_handle()) :: {non_neg_integer(), non_neg_integer()}
   def write_chunk(chunk, db) do
     batch = for [_id, key, value] <- chunk, do: {:put, key, value}
     # :ok = :rocksdb.write(db, batch, sync: true)
@@ -123,7 +180,8 @@ defmodule FileConfigRocksdb.Handler.Csv do
     {length(batch), duration}
   end
 
-  @spec insert_chunk(list(tuple), :ets.tab, :rocksdb.db_handle) :: {non_neg_integer, non_neg_integer}
+  @spec insert_chunk(list(tuple()), :ets.tab(), :rocksdb.db_handle()) ::
+          {non_neg_integer(), non_neg_integer()}
   defp insert_chunk(chunk, tab, db) do
     batch = for {key, value} <- chunk, do: {:put, key, value}
     {duration, :ok} = :timer.tc(:rocksdb, :write, [db, batch, []])
@@ -131,30 +189,4 @@ defmodule FileConfigRocksdb.Handler.Csv do
 
     {length(batch), duration}
   end
-
-  @spec update_db?(Path.t, :calendar.datetime) :: boolean
-  defp update_db?(path, update_mtime) do
-    case File.stat(path) do
-      {:ok, _dir_stat} ->
-        case File.stat(Path.join(path, "CURRENT")) do
-          {:ok, %{mtime: file_mtime}} ->
-            file_mtime < update_mtime
-          {:error, _reason} ->
-            true
-        end
-
-        # case File.ls(path) do
-        #   {:ok, files} ->
-        #     file_times = for file <- files, file_path <- Path.join(path, file),
-        #       {:ok, %{mtime: file_mtime}} <- File.stat(file_path), do: file_mtime
-        #     Enum.all?(file_times, &(&1 < mod))
-        #   {:error, reason} ->
-        #     Logger.warning("Error reading path #{path}: #{reason}")
-        #     true
-        # end
-      {:error, :enoent} ->
-        true
-    end
-  end
-
 end
